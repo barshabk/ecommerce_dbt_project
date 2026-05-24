db5e7b2eba85fb56f54b71e42396692d88ba3e99
Barsha Bishwakarma <barshabishwakarma6@gmail.com>
2026-05-24 15:53:56 -0400

    add mart_order_payments model

---
+++ models/marts/mart_order_payments.sql
@@ -0,0 +1,19 @@
+with order_payments as (
+    select * from {{ ref('stg_brazillian_dbt__order_payments') }}
+),
+
+orders as (
+    select * from {{ ref('stg_brazillian_dbt__orders') }}
+)
+
+select
+    order_payments.order_id,
+    order_payments.payment_type,
+    order_payments.payment_value,
+    order_payments.payment_installments,
+    orders.customer_id,
+    orders.order_status
+from order_payments
+join orders
+    on order_payments.order_id = orders.order_id
+    
