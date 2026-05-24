428827d6dab6ae44f62d759ac63b0fbdd3f5b132
Barsha Bishwakarma <barshabishwakarma6@gmail.com>
2026-05-24 15:30:53 -0400

    add mart_order_products models

---
+++ models/marts/mart_order_products.sql
@@ -0,0 +1,24 @@
+with order_items as (
+    select * from {{ ref('stg_brazillian_dbt__order_items') }}
+),
+
+products as (
+    select * from {{ ref('stg_brazillian_dbt__products') }}
+),
+
+reviews as (
+    select * from {{ ref('stg_brazillian_dbt__order_reviews') }}
+)
+
+select
+    order_items.order_id,
+    order_items.product_id,
+    order_items.seller_id,
+    order_items.price,
+    products.product_category_name,
+    reviews.review_score
+from order_items
+join products
+    on order_items.product_id = products.product_id
+join reviews
+    on order_items.order_id = reviews.order_id
